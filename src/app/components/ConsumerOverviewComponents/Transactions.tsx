71df1b6fc158e40777b9c9a964d299e52fbe05e5
chirag sonar <chiragsonar1718@gmail.com>
2024-01-13 20:57:50 +0530

    feat: created Transactions component which renders the top 5 greated transactions done by the user, the chart needs to added which will show revenue at for multiple time beings like week, month, year and all time

--- src/app/components/ConsumerOverviewComponents/Transactions.tsx
+++ src/app/components/ConsumerOverviewComponents/Transactions.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import Button from "@/app/utils/Button";
+import React from "react";
 
 const Transactions = () => {
   return (
-    <div>Transactions</div>
-  )
-}
+    <div className="flex flex-col gap-8 h-full w-full py-5 px-3 border rounded-md border-border-gray">
+      <div className="flex flex-col gap-3">
+        {/* text and drop down */}
+        <p className="text-lg font-semibold text-text-light-black">
+          5 Greatest Transactions
+        </p>
+        <div className="flex items-center justify-start gap-2">
+          <Button text="Past Week" light={true} className="text-xs h-6" />
+          <Button text="Past Month" light={true} className="text-xs h-6" />
+          <Button text="Past Year" light={true} className="text-xs h-6" />
+          <Button text="All time" light={true} className="text-xs h-6" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Transactions
+export default Transactions;
