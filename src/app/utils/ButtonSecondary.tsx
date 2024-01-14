785e904377205f5029849a7818686ac1c98cbb3d
chirag sonar <chiragsonar1718@gmail.com>
2024-01-14 13:19:10 +0530

    feat: added onClick handler

--- src/app/utils/ButtonSecondary.tsx
+++ src/app/utils/ButtonSecondary.tsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { MouseEventHandler } from "react";
 
 interface ButtonSecondaryProps {
   /**
    * The text content of the button.
    */
   text: string;
+  /**
+   * On Click mouse handleer
+   */
+  onClick?: MouseEventHandler;
 }
 
 /**
@@ -12,9 +16,12 @@ interface ButtonSecondaryProps {
  * @param {ButtonSecondaryProps} props - The properties to configure the button.
  * @returns {JSX.Element} The rendered ButtonSecondary component.
  */
-const ButtonSecondary: React.FC<ButtonSecondaryProps> = ({ text }) => {
+const ButtonSecondary: React.FC<ButtonSecondaryProps> = ({ text, onClick }: ButtonSecondaryProps): JSX.Element => {
   return (
-    <button className="w-[105px] h-auto p-1 text-base font-normal hover:font-bold active:font-bold border-2 border-transparent transition-all duration-100 ease-linear text-text-light-black hover:text-bright-blue active:text-bright-blue hover:border-b-bright-blue active:border-b-bright-blue">
+    <button
+      onClick={onClick}
+      className="w-[105px] h-auto p-1 text-base font-normal hover:font-bold active:font-bold border-2 border-transparent transition-all duration-100 ease-linear text-text-light-black hover:text-bright-blue active:text-bright-blue hover:border-b-bright-blue active:border-b-bright-blue"
+    >
       {text}
     </button>
   );
