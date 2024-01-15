f27b02b4eddcad5f1dfbf267e57ab743773a17db
Chirag Sonar <96980290+chirag1718@users.noreply.github.com>
2024-01-15 21:13:34 +0530

    Update SecondaryDrawer.tsx

--- src/app/manageCustomers/components/Drawer/SecondaryDrawer.tsx
+++ src/app/manageCustomers/components/Drawer/SecondaryDrawer.tsx
@@ -31,7 +31,7 @@ const SecondaryDrawer = () => {
               />
               <div className="flex items-center justify-center h-12 w-12 rounded-full cursor-pointer bg-light-purple">
                 <Image
-                  src="/assets/consumerOverview/moneyBag.svg"
+                  src="/assets/consumerOverview/moneybag.svg"
                   width={18}
                   height={18}
                   alt="cash wallet icon"
